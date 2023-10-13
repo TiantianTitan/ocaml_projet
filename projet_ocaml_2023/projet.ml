72259a8f41fd1cbb751dd46b3b7074750ecd4f87
HaotianXUE <Haotian.Xue@etu.sorbonne-universite.fr>
2023-10-13 21:36:22 +0200

    aujourd'hui fini

--- projet_ocaml_2023/projet.ml
+++ projet_ocaml_2023/projet.ml
@@ -5,6 +5,8 @@
 (**********************Question 1.2 ********************)
 
 let decomposition num =
+  if num = 0 then [false]
+  else
   let rec aux num acc =
     if num <= 0 then acc
     else let modulo = num mod 2 in 
@@ -54,10 +56,12 @@ table 5 8;;
 
 (**********************Question 1.6 ********************)
 
+(*récursive*)
 let rec pow a n =
   if n = 0 then 1
   else a* (pow a (n-1))
 
+  (*récursive terminale*)
 let pow_rec a n =
   let rec aux n acc=
     match n with
@@ -79,6 +83,58 @@ let genAlea n =
   genAlea 100;;
 
 (**********************Question 2.7 ********************)
+
+type decision_binary_tree =
+  | Leaf of bool
+  | Node of int * decision_binary_tree * decision_binary_tree
+
+
+(**********************Question 2.8 ********************)
+
+let rec log2_depth n =
+  if n <= 0 then failwith "n <= 0"
+  else if n = 1 || n = 2 then 1
+  else 1 + log2_depth ((n+1)/2)
+
+(** test: 
+    log2 1 = 1; log2 2 = 1; log2 3= 2; log2 4= 2; log2 5 = 3; log2 8 = 3; log2 9 = 4
+**)
+
+let depth_arbre lb = log2_depth (List.length lb)
+
+
+let cons_chemin_list lb =
+  let rec aux lb n acc =
+    match lb with
+    | [] -> acc
+    | hd :: tl -> aux tl (n+1) (((decomposition n),hd)::acc)
+    in List.rev (aux lb 0 [])
+
+
+
+  let cons_vide_arbre n =
+    
+
+
+
+
+let cons_arbre lb = 
+  let chemin_bool = (cons_chemin_list lb) in
+  let rec aux chemin_bool acc=
+    match chemin_bool with
+    
+
+
+    
+
+
+
+  (**
+  match lb with
+  | [] -> failwith "Empty liste boolean"
+  | [x] -> 
+  | hd :: tl ->
+*)
 
 
 
