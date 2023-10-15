e4a73077461d24a960baffb1ae9a7b0fa7f8b8d7
HaotianXUE <Haotian.Xue@etu.sorbonne-universite.fr>
2023-10-15 23:17:46 +0200

    test commentaire

--- projet_ocaml_2023/projet.ml
+++ projet_ocaml_2023/projet.ml
@@ -12,7 +12,11 @@ let decomposition num =
     else let modulo = num mod 2 in 
       if modulo = 1 then aux (num/2) (true :: acc)
       else aux (num/2) (false :: acc)
-    in List.rev (aux num [])
+    in List.rev (aux num []);;
+
+(**    test   **)
+let ex_dec = decomposition(38);; (*[false; true; true; false; false; true]*)
+(**    test correcte ! **)
 
 (**********************Question 1.3 ********************)
 
@@ -25,11 +29,13 @@ let completion couple =
       | hd :: tl -> aux tl (t-1) (hd::acc)
   in List.rev (aux l t []) 
 
-  (**    test    ****)
+  (****    test  ****)
+  let com_1 = completion([false; true; true; false; false; true], 4)   (*[false; true; true; false]*)
+  let com_2 =  completion([false; true; true; false; false; true], 8)  (*[false; true; true; false; false; true; false; false]*)
 
+  (*  test correcte *)
 
 (**********************Question 1.4 ********************)
-
 
 let composition lb = 
   let rec aux lb acc =
@@ -39,10 +45,11 @@ let composition lb =
                   else aux tl (acc*2)
   in aux (List.rev lb) 0
 
-
 (***test**)
 let listbool = [false;true;true];;
-composition listbool;;
+composition listbool;; (* - : int = 6 *)
+(*  test correcte *)
+
 
   
 (**********************Question 1.5 ********************)
@@ -50,19 +57,19 @@ composition listbool;;
 let table x n = completion ((decomposition x),n);;
 
 (*test*)
-table 15 3;;
-table 12 4;;
-table 5 8;;
+table 15 3;;  (*true; true; true]*)
+table 12 4;;  (*[false; false; true; true]*)
+table 5 8;;   (*[true; false; true; false; false; false; false; false]*)
 
 (**********************Question 1.6 ********************)
 
 (*récursive*)
-let rec pow a n =
+let rec pow_rec a n =
   if n = 0 then 1
-  else a* (pow a (n-1))
+  else a* (pow_rec a (n-1))
 
 (*récursive terminale*)
-let pow_rec a n =
+let pow_rec_t a n =
   let rec aux n acc=
     match n with
     | 0 -> acc  
@@ -72,15 +79,41 @@ let pow_rec a n =
 let genAlea n = 
   let rec aux n acc  =
     if n < 0 then acc
-    else if n < 64 then (decomposition (Int64.to_int (Random.int64 (Int64.of_int (pow 2 n))))) :: acc
+    else if n < 64 then (decomposition (Int64.to_int (Random.int64 (Int64.of_int (pow_rec 2 n))))) :: acc
     else aux (n-64) ((decomposition  (Int64.to_int (Random.int64 Int64.max_int)))::acc)
   in aux n [];;
 
+  let gen61 = (pow_rec 2 61);;  (*int = 2305843009213693952*)
+  let gen62 = (pow_rec 2 62);;  (*int = -4611686018427387904*)
+
+  let gen63 = (pow_rec 2 63);;  (*int = 0*)
   (** test **)
-  genAlea 0;;
-  genAlea 64;;
-  genAlea 65;;
-  genAlea 100;;
+  genAlea 0;;   (*[[false]]*)
+  genAlea 61;;  (*[[false; false; false; true; false; false ;...; true]]*)
+  genAlea 62;;  (*Exception: Invalid_argument "Random.int64".*)
+  genAlea 63;;  (*Exception: Invalid_argument "Random.int64".*)
+  genAlea 64;;  (*[[false];
+                    [true; true; false; true; false; true; true; false; false; false; false; true;
+                    true; false; true; false; false; true; true; false; false; false; false;
+                    true; true; false; true; true; true; false; false; false; false; false;
+                    false; true; false; false; true; true; false; true; true; false; true; true;
+                    false; false; true; false; false; true; true; true; true; true; true; true]]*)
+
+  genAlea 100;;  (* [ [true; false; false; false; true; true; false; false; false; false; false;
+                      false; true; true; true; true; true; true; true; false; true; false; true;
+                      false; false; true; true; false; false; true; true; false; true];
+                              [true; false; false; true; true; false; true; false; false; true; false; true;
+                              true; true; true; true; false; true; true; false; false; false; false; false;
+                              false; true; true; false; false; false; false; false; false; true; true;
+                              true; true; true; true; true; true; false; true; true; true; false; true;
+                              true; true; false; true; true; true; false; false; false; true; true; true;
+                              true; true]]*)
+
+
+(*    test :
+    Pas complétement correcte,
+   En conclusion, 2^62 et 2^63 a déjà dépassé le maximum de type int.
+*)
 
 (**********************Question 2.7 ********************)
 
@@ -97,6 +130,7 @@ let rec log2_depth n =
 (** test: 
     log2 1 = 1; log2 2 = 1; log2 3= 2; log2 4= 2; log2 5 = 3; log2 8 = 3; log2 9 = 4
 **)
+(*  test correcte *)
 
 let depth_arbre lb = log2_depth (List.length lb)
 
@@ -109,12 +143,14 @@ let cons_chemin_list lb =
     | hd :: tl -> let com = List.rev (completion ( (decomposition n), log2_depth length) ) in aux tl (n+1) (  (com ,hd ) :: acc)
     in  List.rev (aux lb 0 []) ;;
 
+(*  test *)
 let chemin_list1 = cons_chemin_list [false;false;true] ;;   
 (* [([false; false], false); ([false; true], false); ([true; false], true)] *)
 let chemin_list2 = cons_chemin_list [true;false;true;true;true];;
 (* 
 [([false; false; false], true); ([false; false; true], false);([false; true; false], true); ([false; true; true], true);([true; false; false], true)] *)
 
+(*  test correcte *)
 
   let cons_vide_arbre n =
     if n = 0 then failwith "Empty tree"
@@ -144,9 +180,13 @@ let rec inserer chemin_bool arbre =
             if hd = false then Node(depth, inserer (tl,boolean) l,r)
             else  Node(depth,l,inserer (tl,boolean) r)  
 
-let ex_inserer = inserer ([false;false],true) (inserer ([true;false],true) ex_tree )  (* insérer 1 leaf et 3 leaf*)
+let ex_inserer = inserer ([false;false],true) (inserer ([true;false],true) ex_tree )  
+
+(*  test  *)
+(* insérer 1 leaf et 3 leaf*)
 (* val ex_inserer : decision_binary_tree =
   Node (1, Node (2, Leaf true, Leaf false), Node (2, Leaf true, Leaf false)) *)
+(*  test correcte *)
 
 let cons_arbre lb = 
   let chemin_bool_list = cons_chemin_list lb in
@@ -158,8 +198,7 @@ let cons_arbre lb =
     | hd :: tl -> inserer_list tl (inserer hd acc)
     in inserer_list chemin_bool_list arbre_init 
 
-
-    (* test correct *)
+(*  test *)
 let ex_list1 = [true;false;true]
 let ex_list2 = [true;true;true;true;false;false;false;false;true]
 
@@ -177,7 +216,8 @@ let ex_cons_arbre2 = cons_arbre ex_list2
     Node (3, Node (4, Leaf true, Leaf false), Node (4, Leaf false, Leaf false)),
     Node (3, Node (4, Leaf false, Leaf false),
      Node (4, Leaf false, Leaf false)))) *)
-    
+
+(*  test correcte *)
 
 (********************** test de la question 2.8  --> 25899 ********************************)
 
@@ -215,6 +255,7 @@ let liste_feuilles_25899 = liste_feuilles dbt_25899
       true; false; true; false; 
       false; true; true; false]  correcte!  *)
 
+(*  test correcte *)
 
 
 (**********************Question 3.10 ********************)
