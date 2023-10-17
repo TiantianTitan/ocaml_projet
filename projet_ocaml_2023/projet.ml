bfacd85fb331b65b7b988f331752c78d7c3272f7
HaotianXUE <Haotian.Xue@etu.sorbonne-universite.fr>
2023-10-17 15:33:10 +0200

    Ex3 cadre fini et on doit continuer à étudier les détails

--- projet_ocaml_2023/projet.ml
+++ projet_ocaml_2023/projet.ml
@@ -1,9 +1,8 @@
 
 (**********************Question 1.1*********************)
-
+type grand_entier = int list
 
 (**********************Question 1.2 ********************)
-
 let decomposition num =
   if num = 0 then [false]
   else
@@ -19,7 +18,6 @@ let ex_dec = decomposition(38) (*[false; true; true; false; false; true]*)
 (**    test correcte ! **)
 
 (**********************Question 1.3 ********************)
-
 let completion couple =
   let l,t = couple in
   let rec aux l t acc =
@@ -32,11 +30,9 @@ let completion couple =
   (****    test  ****)
   let com_1 = completion([false; true; true; false; false; true], 4)   (*[false; true; true; false]*)
   let com_2 =  completion([false; true; true; false; false; true], 8)  (*[false; true; true; false; false; true; false; false]*)
-
   (*  test correcte *)
 
 (**********************Question 1.4 ********************)
-
 let composition lb = 
   let rec aux lb acc =
     match lb with
@@ -56,7 +52,6 @@ let ex_com2 = composition [true;true] (*- : int = 3 *)
 (**********************Question 1.5 ********************)
 
 let table x n = completion ((decomposition x),n)
-
 (*test*)
 let tab1 = table 15 3  (*true; true; true]*)
 let tab2 = table 12 4  (*[false; false; true; true]*)
@@ -84,22 +79,21 @@ let genAlea n =
     else aux (n-64) ((decomposition  (Int64.to_int (Random.int64 Int64.max_int)))::acc)
   in aux n []
 
-  let gen61 = (pow_rec 2 61)  (*int = 2305843009213693952*)
-  let gen62 = (pow_rec 2 62)  (*int = -4611686018427387904*)
+let gen61 = (pow_rec 2 61)  (*int = 2305843009213693952*)
+let gen62 = (pow_rec 2 62)  (*int = -4611686018427387904*)
 
-  let gen63 = (pow_rec 2 63)  (*int = 0*)
-  (** test **)
-  let ex_0 = genAlea 0   (*[[false]]*)
-  let ex_61 = genAlea 61  (*[[false; false; false; true; false; false ;...; true]]*)
-  let ex_62 = genAlea 62  (*Exception: Invalid_argument "Random.int64".*)
-  let ex_63 = genAlea 63  (*Exception: Invalid_argument "Random.int64".*)
-  let ex_64 = genAlea 64  (*[[false];
-                    [true; true; false; true; false; true; true; false; false; false; false; true;
-                    true; false; true; false; false; true; true; false; false; false; false;
-                    true; true; false; true; true; true; false; false; false; false; false;
-                    false; true; false; false; true; true; false; true; true; false; true; true;
-                    false; false; true; false; false; true; true; true; true; true; true; true]]*)
-
+let gen63 = (pow_rec 2 63)  (*int = 0*)
+(** test **)
+let ex_0 = genAlea 0   (*[[false]]*)
+let ex_61 = genAlea 61  (*[[false; false; false; true; false; false ;...; true]]*)
+let ex_62 = genAlea 62  (*Exception: Invalid_argument "Random.int64".*)
+let ex_63 = genAlea 63  (*Exception: Invalid_argument "Random.int64".*)
+let ex_64 = genAlea 64  (*[[false];
+                  [true; true; false; true; false; true; true; false; false; false; false; true;
+                  true; false; true; false; false; true; true; false; false; false; false;
+                  true; true; false; true; true; true; false; false; false; false; false;
+                  false; true; false; false; true; true; false; true; true; false; true; true;
+                  false; false; true; false; false; true; true; true; true; true; true; true]]*)
 let ex_100 = genAlea 100  (* [ [true; false; false; false; true; true; false; false; false; false; false;
                       false; true; true; true; true; true; true; true; false; true; false; true;
                       false; false; true; true; false; false; true; true; false; true];
@@ -117,7 +111,6 @@ let ex_100 = genAlea 100  (* [ [true; false; false; false; true; true; false; fa
 *)
 
 (**********************Question 2.7 ********************)
-
 type decision_binary_tree =
   | Leaf of bool
   | Node of int * decision_binary_tree * decision_binary_tree
@@ -134,8 +127,6 @@ let rec log2_depth n =
 (*  test correcte *)
 
 let depth_arbre lb = log2_depth (List.length lb)
-
-
 let cons_chemin_list lb =
   let length = List.length lb in
   let rec aux lb n acc =
@@ -148,9 +139,7 @@ let cons_chemin_list lb =
 let chemin_list1 = cons_chemin_list [false;false;true]   
 (* [([false; false], false); ([false; true], false); ([true; false], true)] *)
 let chemin_list2 = cons_chemin_list [true;false;true;true;true]
-(* 
-[([false; false; false], true); ([false; false; true], false);([false; true; false], true); ([false; true; true], true);([true; false; false], true)] *)
-
+(* [([false; false; false], true); ([false; false; true], false);([false; true; false], true); ([false; true; true], true);([true; false; false], true)] *)
 (*  test correcte *)
 
 let cons_vide_arbre n =
@@ -166,7 +155,6 @@ let ex_tree = cons_vide_arbre 2
    Node (1, 
       Node (2, Leaf false, Leaf false), 
       Node (2, Leaf false, Leaf false))*)
-
 
 let rec inserer chemin_bool arbre =
   let chemin,boolean = chemin_bool in
@@ -202,12 +190,9 @@ let cons_arbre lb =
 (*  test *)
 let ex_list1 = [true;false;true]
 let ex_list2 = [true;true;true;true;false;false;false;false;true]
-
 let ex_cons_arbre1 = cons_arbre ex_list1 
 (* [true; true; true; true; false; false; false; false; true] *)
-
 let ex_cons_arbre2 = cons_arbre ex_list2 
-
 (* Node (1,
    Node (2,
     Node (3, Node (4, Leaf true, Leaf true), Node (4, Leaf true, Leaf true)),
@@ -221,12 +206,9 @@ let ex_cons_arbre2 = cons_arbre ex_list2
 (*  test correcte *)
 
 (********************** test de la question 2.8  --> 25899 ********************************)
-
 let ex_25899 = decomposition 25899
 (* [true; true; false; true; false; true; false; false; true; false; true; false;false; true; true] *)
-
 let dbt_25899 = cons_arbre ex_25899
-
 (* Node (1,
     Node (2,
       Node (3, Node (4, Leaf true, Leaf true), Node (4, Leaf false, Leaf true)),
@@ -240,7 +222,6 @@ let dbt_25899 = cons_arbre ex_25899
 
 
 (**********************Question 2.9 ********************)
-
 let liste_feuilles arbre =
   let rec aux arbre acc =
     match arbre with
@@ -258,7 +239,6 @@ let liste_feuilles_25899 = liste_feuilles dbt_25899
 
 (*  test correcte *)
 
-
 (**********************Question 3.10 ********************)
 type decision_binary_tree =
   | Leaf of bool
@@ -274,7 +254,6 @@ type elements = {
 type liste_deja_vus = elements list
 
 (**********************Question 3.11 ********************)
-
 (** Utilisation de ref := et ! **)
 let list_vide = ref []    (** val list_vide : '_weak1 list ref = {contents = []} **)
 let _ = list_vide := 1 :: !list_vide  (** - : unit = () **)
@@ -284,7 +263,6 @@ let list = list_vide  (** - : int list ref = {contents = [1]} **)
 (** Préparation de la fonction compressionParListe**)
 
 (** 1. liste_feille_to_ge **)
-
 let rec sup_prefix0 l =
   match l with
     | [] -> []
@@ -304,7 +282,6 @@ let liste_feuille_to_ge lf =
     | false -> aux (List.rev (completion ((List.rev lf),(list_length - 64)))) ((composition (completion (lf,64))) ::acc)
     (*** explication:  si length > 64, acc @ les premiers 64 ensuite on la reste sauf que les premiers 64 va continuer la récursive ***)
   in aux lf []
-
 (** test liste_feuille_to_ge  est juste derrière que le test de ge_to_liste_feuille **)
 
 (** 2. ge_to_liste_feuille **)
@@ -331,10 +308,8 @@ let ex_dec_ge2 = decomposition_ge [2;1]
 (** test correct **)
 
 let ge_to_liste_feuille ge = liste_feuilles (cons_arbre (decomposition_ge ge))
-
 (** test ge_to_liste_feuille **)
 let ex_ge_to_fl1 = ge_to_liste_feuille [2;1] 
-
 (* 
 [false; true; false; false; false; false; false; false; false; false; 
  false;false; false; false; false; false; false; false; false; false; 
@@ -352,9 +327,7 @@ let ex_ge_to_fl1 = ge_to_liste_feuille [2;1]
 
 let ex_ge_to_fl2 = ge_to_liste_feuille [1] 
 
-
 (** test liste_feuille_to_ge **)
-
 let length1 = List.length (ge_to_liste_feuille [1])                 (* 2 *)
 let ex_lf_to_ge1 = liste_feuille_to_ge (ge_to_liste_feuille [1])         (* - : int list = [1] *)
 let length2 = List.length (ge_to_liste_feuille [2;1])               (* 128 *)
@@ -367,15 +340,13 @@ let ex_lf_to_ge5 = liste_feuille_to_ge (ge_to_liste_feuille [5;4;3;2;1]) (* - : 
 
  (** test correct **)
 
+(** 3. check si n est la première composante d’un couple stocké dans ListeDejaVus **)
 
- (** 3. check si n est la première composante d’un couple stocké dans ListeDejaVus **)
-
-  let rec check_ldv n ldv =
-    match ldv with
-    | [] -> false
-    | hd :: tl ->  if  n = hd.entier then true
-    else check_ldv n tl
-
+let rec check_ldv n ldv =
+  match ldv with
+  | [] -> false
+  | hd :: tl ->  if  n = hd.entier then true
+  else check_ldv n tl
 
 (** Utilisation de type structure et instanciation **)
 type test = {
@@ -388,33 +359,52 @@ let a = {
   test2 = "Hello";
 }
 
-
 (* test *)
 let ex_node = {id = 0} 
 let ex_element = { entier = [1;2;3] ; node = ex_node ; } 
-
 let ex_ldv = [ex_element] 
-
 let ex_check1 = check_ldv [1;2;3] ex_ldv  (*true*)
 let ex_check2 = check_ldv [1;2] ex_ldv    (*false*)
 (* test correcte! *)
 
+let child_noeud elem l_ref =
+  let l = !l_ref in
+  let rec aux l =
+  match l with
+  | [] -> failwith "no child with the node"
+  | hd :: tl -> 
+    if hd.entier = elem then hd.node 
+    else aux tl
+    in aux l
+
+ (** test **)
+
+let ex_ldv_ref = ref [{entier = [1;2]; node = {id = 1}};{entier = [1;2;3]; node = {id = 2}}]
+let ex_child_noeud = child_noeud  [1;2] ex_ldv_ref
 
 let maj_noeud elem noeud l_ref =
     let l = !l_ref in
+    let ret_ref = ref {id = -1} in
     let rec aux l acc=
       match l with
       | [] -> acc
       | { entier = e ;node = n } :: tl -> 
-        if e = elem then aux tl ({entier = e; node = noeud} :: acc)
+        if e = elem then let _ = ret_ref := n  in
+        aux tl ({entier = e; node = noeud} :: acc)
         else   aux tl ({ entier = e ;node = n } :: acc)
-    in l_ref :=  (aux l [])
+    in let _ = l_ref :=  (aux l []) in !ret_ref
 
 (** test_maj_noeud **)
 let ex_ldv_ref = ref [{entier = [1;2]; node = {id = 1}};{entier = [1;2;3]; node = {id = 2}}]
 let ex_maj_noeud = maj_noeud [1;2] {id = 3} ex_ldv_ref
 
 let deja_maj = !ex_ldv_ref  (* [{entier = [1; 2; 3]; node = {id = 2}}; {entier = [1; 2]; node = {id = 3}}] *)
+(** test correcte! **)
+
+(* let maj_parent_noeud elem noeud l_ref =
+  let child_noeud = maj_noeud elem noeud l_ref in
+   *)
+
 
 (*Le pricipale de la fonction compressionParListe*)
 let compressionParListe arbre_decision =
@@ -422,16 +412,83 @@ let compressionParListe arbre_decision =
   let rec aux arbre_decision =
     let ge = liste_feuille_to_ge (liste_feuilles arbre_decision) in
     match arbre_decision with
-    | Leaf (b) -> failwith "arbre_decision est une feuille"
-    | Node(depth,l,f) -> 
+    | Leaf (b) -> ()
+    | Node(depth,l,r) -> 
         if check_ldv ge !listeDejaVus_ref then 
-        failwith "todo"
-
+        let _ = maj_noeud ge {id = depth} listeDejaVus_ref in let _ = aux l in aux r   
         else  
-          let new_node = { id = 0 } in
+          let new_node = { id = depth } in
           let element = {entier = ge; node = new_node;} in
-          listeDejaVus_ref := (element) :: !listeDejaVus_ref
-  in aux arbre_decision
+          let _ =listeDejaVus_ref := (element) :: !listeDejaVus_ref in let _ = aux l in aux r
+  in let _ = aux arbre_decision in listeDejaVus_ref 
+
+(** test **)
+let ex_25899 = decomposition 25899
+let dbt_25899 = cons_arbre ex_25899
+let test_compression_par_liste_25899 = let ret = compressionParListe dbt_25899 in !ret
+
+
+(** test not end and try to understand l'algo ZDD **)
+
+
+
+
+(**********************Question 3.12 ********************)
+
+(** Utilisation de ficher en ocaml **)
+let file_test = "./projet_ocaml_2023/test.dot"
+
+let () = 
+  let message = "hello world\ntest\n" in
+  let oc =open_out file_test in
+  Printf.fprintf oc "%s\n" message;
+  close_out oc
+
+
+(** l'implémentation de sémantique de dot **)
+(* let cons_graphe_dot arbre_de_decision = "TEST\n" *)
+let cons_graphe_dot arbre_de_decision = failwith "todo"
+
+(** Le principale de construction d'un ficher dot **)    
+let file_graphe = "./projet_ocaml_2023/graphe.dot"
+let dot fichier arbre_de_decision = 
+  let _ = () in 
+    let graphe = cons_graphe_dot arbre_de_decision in
+    let oc = open_out file_graphe in
+    Printf.fprintf oc "%s\n" graphe;
+    close_out oc
+    
+let test_graphe = dot file_graphe dbt_25899
+     
+
+
+
+
+(**********************Question 3.13 ********************)
+(* On a déjà fait cette question quand on teste la question 2.8 *)
+let ex_25899 = decomposition 25899
+(* [true; true; false; true; false; true; false; false; true; false; true; false;false; true; true] *)
+let dbt_25899 = cons_arbre ex_25899
+(* val dbt_25899 : decision_binary_tree/2 =
+Node (1,
+  Node (2,
+  Node (3, Node (4, Leaf true, Leaf true), Node (4, Leaf false, Leaf true)),
+  Node (3, Node (4, Leaf false, Leaf true), Node (4, Leaf false, Leaf false))),
+  Node (2,
+  Node (3, Node (4, Leaf true, Leaf false), Node (4, Leaf true, Leaf false)),
+  Node (3, Node (4, Leaf false, Leaf true), Node (4, Leaf true, Leaf false)))) *)
+
+  (** C'est exactement la même arbre que la figure 1 dans l'annoncé**)
+
+
+(**********************Question 3.14 ********************)
+
+(******  TODO: comprendre Q3.11 et Q3.12 ici c'est juste vérifier  *****)
+
+
+
+
+
 
 
 
--- projet_ocaml_2023/test.ml
+++
@@ -1,8 +0,0 @@
-let x = 1 in x
-
-
-
-let rec fuck a b = 
-  match a with
-  | [] -> b
-  | hd :: tl -> hd + fuck tl b 
